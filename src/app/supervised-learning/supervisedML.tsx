40c50ca76d17169464a53eddae19b2d38ff7571d
Billeh <animusmion@gmail.com>
2025-08-31 01:19:51 -0500

    Mid-way with Finishing the Linear Regression section

--- src/app/supervised-learning/supervisedML.tsx
+++ src/app/supervised-learning/supervisedML.tsx
@@ -1,6 +1,9 @@
+import 'katex/dist/katex.min.css';
+import {BlockMath} from 'react-katex';
 import {SupervisedChart} from './supervisedChart'
 
 export function SupervisedML() {
+  var Latex = require('react-latex');
   return (
     <div className="flex flex-row lg:flex-row w-full min-h-screen px-6 py-8 gap-10">
       {/*Text Box */}
@@ -19,8 +22,21 @@ export function SupervisedML() {
           The idea of linear regression comes from the concept of line of best fit, used in many scientific fields more than just computer science: Chemistry, Physics,
           Economics, Psychology, etc. Any fields that has experimented with data has heard of drawing line of best fit to simplify the relationship between two continuous
           variables.
-
         </p>
+  
+        <p className='mt-3'>
+          As the name states, our line of best fit will be linear - it will follow the standard y = wx + b, with slope w and intercept b. 
+          In machine learning, we deal with accuracy a lot. How accurate is our line of best fit? Is it a good representation of our points trend? To combat that, we measure something
+          called the Cost Function, or J(w, b). J(w,b) measures the "error" between our actual data points and the predicted points. Our goal is always to minimize J(w, b).
+        </p>
+
+        <BlockMath math="J(w, b) = \frac{1}{2m} \sum^{m}_{i=1} {(\hat{y}^{(i)} - y^{(i)})^2} = \frac{1}{2m} \sum^{m}_{i=1} {{(wx^{(i)} + b)} - y^{(i)})^2}" />
+
+        <p className='mt-3'>
+          The formula above is one of the simple Square Cost Function. In the formula, <BlockMath math='\hat{y}^{(i)} = wx^{(i)} + b'/> represent the predicted y value when an x value input is given.
+          We can alter the values of w and b to change the values of J(w, b), and try to minimize it.
+        </p>
+
       </div>
 
       {/* Graph */}
